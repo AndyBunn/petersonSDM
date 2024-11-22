641bffd5397cfec049763811dd38c8f7e5eca68c
AndyBunn <bunna@wwu.edu>
2024-11-21 20:03:30 -0800

    pushing new changes -- esp to the prep data script.

--- 00_prep_data.R
+++ 00_prep_data.R
@@ -1,3 +1,28 @@
+################################################################################
+#
+# AGB Nov 2024
+#
+# A script that takes presence data as lat/long points from MP and fetches
+# bioClim (present and future) and topo data at 30 sec res for
+# a (user-defined) box around those points
+#
+# We create pseudo-absence data to match the presence data above an
+# elevation threshold and make sure that absences aren't on the same cells
+# as the presence data.
+#
+# We then export the presence/absence data and the bricks that we'll use for
+# the modeling. These bricks use the bioclim variables as well as topo and
+# tree cover data as predictors.
+#
+# Inputs:
+# Chariclea_Final_Thin.csv
+# Outputs:
+#   presAbsSF.rds -- presAbs data plus predictors as class sf (mapping is easy)
+#   presAbsDF.rds -- presAbs data plus predictors as stright data.frame
+#   predictorsModern.rds -- the brick of modern climate and topo stuff
+#   predictorsFuture.rds -- the brick of future climate and topo stuff
+#
+################################################################################
 # Clear the R environment of all objects to start fresh
 rm(list=ls())
 # set a seed for reprodicibility of the random sampling
@@ -39,16 +64,18 @@ if (!dir.exists(data_path)) {
 }
 
 # Now we get the bioClim variables from world clim. Note that this
-# is a big(ish) ask the first time you do it (600MB).
+# is a big ask the first time you do it since the data are global at
+# 30 sec res. Many GB of data.
+
 # But after the data are initially
-# downloaded they live on your machine and don't need to be dowloaded again
+# downloaded they live on your machine and don't need to be downloaded again
 # provided you are in the same project
-# After it's downloaded we will clip it to to a more reasobale area
+# After it's downloaded we will clip it to to a more reasonable area
 #
 # more info on variables here:
 # https://www.worldclim.org/data/bioclim.html
 bioClim <- worldclim_global(var = "bio",
-                            res = 2.5,
+                            res = 0.5,
                             path = data_path)
 # note the class (SpatRaster) and that this is a 19 layer cube
 bioClim
@@ -75,21 +102,14 @@ bioClim
 # BIO19 = Precipitation of Coldest Quarter
 
 bioClimNames <- c("AnnualMeanTmp", "MeanDiurnalRange", "Isothermality", "TmpSeasonality",
-                  "MaxTmpPrcpWarmestMonth", "MinTmpPrcpColdestMonth", "TmpAnnualRange",
-                  "MeanTmpPrcpWettestQtr", "MeanTmpPrcpDriestQtr", "MeanTmpPrcpWarmestQtr",
-                  "MeanTmpPrcpColdestQtr", "AnnualPrcp","PrcpPrcpWettestMonth",
-                  "PrcpPrcpDriestMonth", "PrcpSeasonality", "PrcpPrcpWettestQtr",
-                  "PrcpPrcpDriestQtr", "PrcpPrcpWarmestQtr", "PrcpPrcpColdestQtr")
+                  "MaxTmpWarmestMonth", "MinTmpColdestMonth", "TmpAnnualRange",
+                  "MeanTmpWettestQtr", "MeanTmpDriestQtr", "MeanTmpWarmestQtr",
+                  "MeanTmpColdestQtr", "AnnualPrcp","PrcpWettestMonth",
+                  "PrcpDriestMonth", "PrcpSeasonality", "PrcpWettestQtr",
+                  "PrcpDriestQtr", "PrcpWarmestQtr", "PrcpColdestQtr")
 
 # add names to bioClim
 names(bioClim) <- bioClimNames
-
-# Let's grab the elev data too. We will use it as a predictor and to
-# constrain the psuedo absence point generation. We don't want ridiculous
-# absence points in the lowlands
-elev <- elevation_global(res = 2.5,
-                         path = data_path)
-names(elev) <- "elev"
 
 # Now we get the future climate data from cmip6.
 # you'll want to think about time, model, and ssp choice
@@ -99,10 +119,57 @@ cmip6 <- cmip6_world(model = "HadGEM3-GC31-LL",
                      ssp = "245",
                      time = "2061-2080",
                      var = "bioc",
-                     res = 2.5,
+                     res = 0.5,
                      path = data_path)
 # add in the names
 names(cmip6) <- bioClimNames
+
+# Let's grab the elev data too. We will use it as a predictor and to
+# constrain the psuedo absence point generation. We don't want ridiculous
+# absence points in the lowlands
+elev <- elevation_global(res = 0.5,
+                         path = data_path)
+names(elev) <- "elev"
+
+# we can get slope and aspect as predictors as well. Be aware of the perils of
+# aspect as a predictor though -- 360==0 so typically aspect is transformed to
+# a categorical variable (N,NE,E,SE,S,SW,W,NW) or in the N Hemi to be
+# "southness" -- kind of a poorman's radiation proxy. I'll do both.
+
+slope <- terrain(elev, v = "slope", unit = "degrees")  # Slope in degrees
+aspect <- terrain(elev, v = "aspect", unit = "degrees") # Aspect in degrees
+
+# Define breaks and labels for cardinal directions
+# Aspect in degrees: 0 = North, 90 = East, 180 = South, 270 = West
+categories <- data.frame(
+  from = c(0, 22.5, 67.5, 112.5, 157.5, 202.5, 247.5, 292.5, 337.5),
+  to   = c(22.5, 67.5, 112.5, 157.5, 202.5, 247.5, 292.5, 337.5, 360),
+  value = 1:9,
+  categoricalDirection = c("North", "Northeast", "East", "Southeast", "South",
+            "Southwest", "West", "Northwest", "North")
+)
+
+# Create a categorical SpatRaster from aspect
+aspect_cat <- classify(aspect, rcl = categories[, 1:3], right = FALSE,others=9)
+levels(aspect_cat) <- categories[,3:4]
+
+# Transform aspect to proxy solar radiation 1 is S, 0 are EW, and -1 is N
+# Convert degrees to radians: radians = degrees * pi / 180
+transformed_aspect <- cos(aspect * pi / 180)
+names(transformed_aspect) <- "southness"
+
+# let's put all the topographic data together. I'll not going to include raw
+# aspect here because it's a dangerous predictor variable.
+# Use aspect_cat and/or transformed_aspect as predictors
+topo <- c(elev,slope,aspect,aspect_cat,transformed_aspect)
+
+# Get landcover (it's native as 30 sec values). With `geodata` we can get pct
+# of "trees", "grassland", "shrubs", "cropland", "built", "bare", "snow",
+# "water", "wetland", "mangroves", "moss". I'll use percentage of tree
+# cover which will work best there I think. These are continuous data which are
+# handy as well.
+pctTree <- landcover(var = "trees",
+                     path = data_path)
 
 # We don't want to model the entire world, right?
 # Let's get the extent of the data and expand it by 5 degrees for clipping
@@ -123,9 +190,10 @@ ggplot() +
   geom_sf(data=presenceSF_bbox_expandedSF) +
   geom_sf(data=presenceSF)
 
-# now we can clip the climate data to that box
+# now we can clip the raster data to that box
 bioClim <- crop(x = bioClim, y = presenceSF_bbox_expandedSF)
-elev <- crop(x = elev, y = presenceSF_bbox_expandedSF)
+topo <- crop(x = topo, y = presenceSF_bbox_expandedSF)
+pctTree <- crop(x = pctTree, y = presenceSF_bbox_expandedSF)
 cmip6 <- crop(x = cmip6, y = presenceSF_bbox_expandedSF)
 
 # note the dimensions now -- much smaller
@@ -133,20 +201,20 @@ bioClim
 
 # and take a look -- use elevation as base
 ggplot() +
-  geom_spatraster(data=elev) +
+  geom_spatraster(data=topo$elev) +
   geom_sf(data=presenceSF) +
   scale_fill_hypso_c()
 
-# I anticipate that we will want elevation as a predictor variable given the
-# spp in question. Let's add elevation and the bioclim data together to make the
+# Let's add bioclim, topo, pctTree data together to make the
 # brick that we will use to build the model
-predictorsModern <- c(bioClim,elev)
+predictorsModern <- c(bioClim,topo,pctTree)
 dim(predictorsModern)
 
 # and we will make a brick for future climate to use later. Note that this
-# is the cmip6 data and elevation.
-predictorsFuture <- c(cmip6,elev)
+# is the cmip6 data plus the topo data and pctTree as above.
+predictorsFuture <- c(cmip6,topo,pctTree)
 dim(predictorsFuture)
+# note that the dimensions match
 
 # with our climate data done, we now need to generate the
 # pseudo absence data. Let's do this in a way that constrains the points to
@@ -156,11 +224,12 @@ dim(predictorsFuture)
 presenceSF$elev <- extract(elev,presenceSF)$elev
 # constrain to >5% of obecserved elevations to avoid funnies in the data
 minElev <- quantile(presenceSF$elev, probs = 0.05)
-minElev # about 1000m here
+minElev # about 1100m here
 
 # now create a mask of areas in the box that are > minElev
 absenceMask <- ifel(elev > minElev, elev, NA)
-absenceMask <- crop(absenceMask,presenceSF_bbox) # clip to areas near presence data
+# We will clip this so that the absence data are in areas near presence data
+absenceMask <- crop(absenceMask,presenceSF_bbox)
 # And mask out points that have observations.
 # This is a little cryptic, but basically we are getting the cell IDs where
 # there are presence observations. There are simpler ways (or less confusing)
@@ -178,20 +247,24 @@ pseudoAbsenceSF <- as_sf(pseudoAbsenceSF)
 
 # Combine the pseudoAbsenceSF data and presenceSF data to the make one sf
 # object coded as pres/abs by adding a new column
-presenceSF$present <- TRUE
-pseudoAbsenceSF$present <- FALSE
+presenceSF$present <- "present"
+pseudoAbsenceSF$present <- "absent"
 # and combining -- note we will drop elevation from these data.frames
+# we used elev here to get the right threshold for generating the data
+# but we don't need it in the data frame now.
 presAbsSF <- rbind(presenceSF %>% select(-elev),
                    pseudoAbsenceSF %>% select(-elev))
-presAbsSF
+presAbsSF$present <- as.factor(presAbsSF$present)
+# note that we have the same number of present and absent values
+table(presAbsSF$present)
 
-# and take a look
+# let's take a look
 tm_shape(presAbsSF) +
   tm_symbols(col = "present",style="cat",alpha=0.5)
 
 # now that we have somewhat realistic presence and absence data we can
 # extract the climate data from the and add it to presAbsSF
-presAbsSF <- as_sf(extract(bioClim,presAbsSF,bind=TRUE))
+presAbsSF <- as_sf(extract(predictorsModern,presAbsSF,bind=TRUE))
 presAbsSF # look at all those lovely predictors
 # And convert to a plain data.frame
 presAbsDF <- st_drop_geometry(presAbsSF)
@@ -199,6 +272,9 @@ presAbsDF <- st_drop_geometry(presAbsSF)
 # we are now ready to move on to model development.
 # let's save what I think we'll need
 saveRDS(object = presAbsSF, file = "presAbsSF.rds")
-saveRDS(object = presAbsDF,, file = "presAbsDF.rds")
-saveRDS(object = predictorsModern, file = "predictorsModern.rds")
-saveRDS(object = predictorsFuture, file = "predictorsFuture.rds")
+saveRDS(object = presAbsDF, file = "presAbsDF.rds")
+# we will jack up the compression here to get the files <100MB so they go to
+# the github repo smoothly. bzip2 gets them small but takes longer
+saveRDS(object = predictorsModern, file = "predictorsModern.rds", compress = "bzip2")
+saveRDS(object = predictorsFuture, file = "predictorsFuture.rds", compress = "bzip2")
+
--- 01_build_model.R
+++ 01_build_model.R
@@ -15,6 +15,8 @@ library(tmap)
 library(geodata)
 # Load predicts for doing k-fold
 library(predicts)
+# Load randomForest for doing a rf prediction
+library(randomForest)
 
 # load data
 presAbsSF <- readRDS(file = "presAbsSF.rds")
@@ -22,30 +24,67 @@ presAbsDF <- readRDS(file = "presAbsDF.rds")
 predictorsModern <- readRDS(file = "predictorsModern.rds")
 predictorsFuture <- readRDS(file = "predictorsFuture.rds")
 
-## STOP. Here there be dragons
+# There are typically a few steps to approaching modeling.
+# The first usually consists on deciding about the model type
+# e.g., a glm or a ml approach. Sometimes this involves selecting
+# predictors or evaluating a model for a balance between underfitting
+# and over fitting. I'll leave that distinction up to you of course.
+# In this case we are predicting a binary response. That is a
+# classification exercise as opposed to a regression exercise. There are
+# many kinds of models to predict classes and I'd steer you to the caret
+# package for ways of doing classification (pres/abs) in semi automated
+# ways. But until then...
 
+# When we build the model we aim to use various subsets of data to train
+# the model and then predict it over subsets of the data that the model
+# hasn't seen before. This is testing.
 
-# now we can get a training/test split
+# the approach we can use here is to use a certain number (k) of folds of the
+# data. Randomly we assign each each row in the data to a fold. Each fold
+# serves as training data and all the other folds are used to test the model
+# We record the efficacy of the model and repeat so that each row gets a chance
+# to be both training and testing data. Let's make the folds. We can do this
+# "by hand" with the sample function or use any number of other functions in
+# ml or regression packages to help us. I'll show a way here using the folds
+# function predicts package.
+
+# create five folds in the data
 fold <- folds(x = presAbsDF,
               k = 5,
               by = presAbsDF$present)
+
 # our first (simple) split can just be using fold==1 to test the
 # model and all the other folds to train it
-testing <- presAbsDF[fold == 1, ]
-training <- presAbsDF[fold != 1, ]
 
-# and we can build a model -- e.g., a glm with binom
-simpleModel <- glm(present ~ ., data = presAbsDF, family = binomial())
+## Fold 1
+testing <- presAbsDF[fold == 1,]
+training <- presAbsDF[fold != 1,]
+
+# note that all or data are accounted for
+length(presAbsDF$present)
+length(training$present) + length(testing$present)
+table(training$present)
+table(testing$present)
+
+# and we can build a model -- e.g., a randomForest model
+simpleModel <- randomForest(present ~ ., data = training)
+simpleModel # note the table that shows the accuracy of the predictions
+# we can also see which variables are important to the model:
+varImpPlot(simpleModel)
+partialPlot(simpleModel, as.data.frame(training), elev)
+
+# how well does the model do on the withheld data?
+fitsFold1 <- predict(simpleModel,testing)
+# let's make table similar to that made by randomForest
+# make df and mutate in the error
+accuracyFold1 <- table(fitsFold1,testing$present)
+
 # and get predictions from it -- note that this is built from just
 # the training data which is a small set of the whole data set
-
 
 simpleModelFits <- predict(predictorsModern, simpleModel, type = "response")
 # this is SpatRaster showing the prob of spp being present
 simpleModelFits
-ggplot() + geom_histogram(data=simpleModelFits, mapping = aes(x=lyr1))
-# let's make it categorical
-simpleModelFitsCategorical <- as.factor(ifel(simpleModelFits > 0.5, TRUE, FALSE))
 # and do a simple plot
-ggplot() + geom_spatraster(data=simpleModelFitsCategorical)
+ggplot() + geom_spatraster(data=simpleModelFits)
 
